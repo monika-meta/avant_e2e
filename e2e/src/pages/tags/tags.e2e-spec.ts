3778684e35b1c32501fb3e7623d184335fe4ffdb
Monika <monika.mathur@meta.com>
2019-10-15 17:25:27 +0530

    completed tags

--- e2e/src/pages/tags/tags.e2e-spec.ts
+++ e2e/src/pages/tags/tags.e2e-spec.ts
@@ -4,26 +4,25 @@ import { USERNAME, PASSWORD } from '../../e2e-config';
 import { TagsPage } from './tags.po';
 import { HomePage } from '../home/home.po';
 
-describe('Conversation', () => {
+describe('tags', () => {
 
     let tagsPage: TagsPage;
     let util: UtilPage;
     let home: HomePage;
 
     beforeAll(() => {
-
         tagsPage = new TagsPage();
         util = new UtilPage();
         home = new HomePage();
         browser.ignoreSynchronization = true;
-
         browser.waitForAngular();
-        util.waitForElementToBeVisible(home.getHomeNavFileManagerEle());
-        expect(home.getHomeNavFileManagerEle()).toBeTruthy();
-        home.getHomeNavFileManagerEle().click();
+        util.waitForElementToBeVisible(home.getHomeNavTagEle());
+        expect(home.getHomeNavTagEle()).toBeTruthy();
+        home.getHomeNavTagEle().click();
     });
 
     it('Validate UI of Tag when user clicks on the  Tag from the left pane', () => {
+        browser.sleep(4000);
         expect(tagsPage.getSpaceName()).toBeTruthy();
         expect(tagsPage.getAddNewTagCategoryButton()).toBeTruthy();
         expect(tagsPage.getSearchCategoryInputBox()).toBeTruthy();
@@ -34,33 +33,56 @@ describe('Conversation', () => {
     });
 
     it('Validate screen of status when no records are present ', () => {
+        // util.waitForElementToBeVisible(tagsPage.getAddNewTagCategoryButton());
+        expect(tagsPage.getAddNewTagCategoryButton()).toBeTruthy();
         tagsPage.getAddNewTagCategoryButton().click();
+        util.waitForElementToBeVisible(tagsPage.getAddCategoryInput());
         expect(tagsPage.getAddCategoryInput()).toBeTruthy();
-        tagsPage.getAddCategoryInput().sendText('test');
+        tagsPage.getAddCategoryInput().sendKeys('test2');
         expect(tagsPage.getAddCategoryCheckIcon()).toBeTruthy();
+        browser.sleep(2000);
         tagsPage.getAddCategoryCheckIcon().click();
-        expect(tagsPage.getCategoryAddedRow('test')).toBeTruthy();
-        expect(tagsPage.getCategoryAddedRowEdit()).toBeTruthy();
-        tagsPage.getCategoryAddedRowEdit().click();
-        tagsPage.getCategoryAddedRowDescriptionColInput().click();
-        tagsPage.getCategoryAddedRowDescriptionColInput().sendText('test Description');
-        expect(tagsPage.getCategoryAddedRowSave()).toBeTruthy();
-        tagsPage.getCategoryAddedRowSave().click();
+        browser.sleep(2000);
+        browser.sleep(2000);
 
+        expect(tagsPage.getCategoryAddedRow('test2')).toBeTruthy();
+        browser.sleep(2000);
+        browser.sleep(2000);
+        expect(tagsPage.getCategoryAddedRowEdit()).toBeTruthy();
+        browser.actions().mouseMove(tagsPage.getCategoryAddedRowEdit().last()).perform();
+        browser.sleep(1000);
+
+        tagsPage.getCategoryAddedRowEdit().last().click();
+        browser.sleep(1000);
+        tagsPage.getCategoryAddedRowDescriptionColInput().click();
+        browser.sleep(1000);
+        tagsPage.getCategoryAddedRowDescriptionColInput().sendKeys('test Description');
+        expect(tagsPage.getCategoryAddedRowSave()).toBeTruthy();
+        browser.sleep(1000);
+        tagsPage.getCategoryAddedRowSave().click();
+        browser.sleep(4000);
 
     });
+
     it('Verify functionality of edit icon when user clicks on it Verify functionality of when user clicks on arrow of category  ', () => {
         expect(tagsPage.getCategoryAddedRowArrow()).toBeTruthy();
         tagsPage.getCategoryAddedRowArrow().click();
+        browser.sleep(4000);
         expect(tagsPage.getCollapsedCategoryTr()).toBeTruthy();
         expect(tagsPage.getCollapsedCategoryTrAddBtn()).toBeTruthy();
         tagsPage.getCollapsedCategoryTrAddBtn().click();
-        tagsPage.getCollapsedCategoryTrInput().sendText('test Tag');
+        browser.sleep(2000);
+        tagsPage.getCollapsedCategoryTrInput().sendKeys('test Tag');
+        browser.sleep(1000);
         tagsPage.getCollapsedCategoryTrDescriptionTd().click();
-        tagsPage.getCollapsedCategoryTrInput().sendText('test tag description');
+        browser.sleep(2000);
+        tagsPage.getCollapsedCategoryTrInput().sendKeys('test tag description');
         tagsPage.getCollapsedCategoryTrEnabledCheckbox().click();
+        browser.sleep(2000);
         tagsPage.getCollapsedCategoryTrVisibleCheckbox().click();
+        browser.sleep(2000);
         expect(tagsPage.getCategoryAddedRowSave()).toBeTruthy();
         tagsPage.getCategoryAddedRowSave().click();
+        browser.sleep(2000);
     });
 });
--- e2e/src/pages/tags/tags.po.ts
+++ e2e/src/pages/tags/tags.po.ts
@@ -8,7 +8,7 @@ export class TagsPage {
         return element(by.css('p[_ngcontent-gbg-c34][class="space-name"]'));
     }
     getAddNewTagCategoryButton() {
-        return element(by.xpath('(//tr[contains(@class,"dx-header-row")]/td//div/p[contains(text(),"Category Name")])[1]]'));
+        return element(by.xpath('//button[contains(@class,"new-tagcategory-form-button")]'));
     }
     getSearchCategoryInputBox() {
         return element(by.xpath('//*[@id="mat-tab-content-0-2"]/div[1]/channel-status[1]/div[1]/div[1]/button[2]'));
@@ -26,16 +26,16 @@ export class TagsPage {
         return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[@aria-describedby="dx-col-41"][text()="${categoryName}"]`))
     }
     getCategoryAddedRowDescriptionColInput() {
-        return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[@aria-describedby="dx-col-42"]//input`))
+        return element.all(by.xpath(`//tr[1][contains(@class,"dx-edit-row")][1]//div[@class="dx-texteditor-container"]//input`)).get(1);
     }
     getCategoryAddedRowEdit() {
-        return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Edit"]`));
+        return element.all(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Edit"]`));
     }
     getCategoryAddedRowDelete() {
-        return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Delete"]`));
+        return element.all(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Delete"]`));
     }
     getCategoryAddedRowSave() {
-        return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Save"]`));
+        return element.all(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-edit")]//a[@title="Save"]`)).last();
     }
     getCategoryAddedRowArrow() {
         return element(by.xpath(`//tr[1][contains(@class,"dx-column-lines")]//td[contains(@class,"dx-command-expand")][@role="gridcell"]//div[@class="dx-datagrid-group-closed"]`));
@@ -47,20 +47,18 @@ export class TagsPage {
         return element(by.xpath(`//div[@class="dx-button-content"]//i[@class="dx-icon dx-icon-edit-button-addrow"]`));
     }
     getCollapsedCategoryTrInput() {
-        return element(by.xpath(`//td[@aria-describedby="dx-col-443"]//div[@class="dx-texteditor-input-container"]//input[@class="dx-texteditor-input"]`));
+        return element.all(by.xpath(`//tr[@aria-rowindex="1"][contains(@class,"dx-edit-row")]//div[@class="dx-texteditor-input-container"]//input`)).last();
     }
     getCollapsedCategoryTrDescriptionTd() {
-        return element(by.xpath(`//td[@aria-describedby="dx-col-443"]//div[@class="dx-texteditor-input-container"]//input[@class="dx-texteditor-input"]`));
+        return element.all(by.xpath(`//tr[@aria-rowindex="1"][contains(@class,"dx-edit-row")]//div[@class="dx-texteditor-input-container"]//input`)).get(1);
     }
     getCollapsedCategoryTrDiscriptionInput() {
-        return element(by.xpath(`//td[@aria-describedby="dx-col-443"]//div[@class="dx-texteditor-input-container"]//input[@class="dx-texteditor-input"]`));
+        return element.all(by.xpath(`//tr[@aria-rowindex="1"][contains(@class,"dx-edit-row")]//div[@class="dx-texteditor-input-container"]//input`)).get(1);
     }
     getCollapsedCategoryTrEnabledCheckbox() {
-        return element(by.xpath(`//td[@aria-describedby="dx-col-445"]//div[@class="dx-texteditor-input-container"]//input[@class="dx-texteditor-input"]`));
+        return element.all(by.xpath(`//tr[@aria-rowindex="1"][contains(@class,"dx-edit-row")]//div[@class="dx-checkbox-container"]`)).first();
     }
     getCollapsedCategoryTrVisibleCheckbox() {
-        return element(by.xpath(`//td[@aria-describedby="dx-col-446"]//div[@class="dx-texteditor-input-container"]//input[@class="dx-texteditor-input"]`));
+        return element.all(by.xpath(`//tr[@aria-rowindex="1"][contains(@class,"dx-edit-row")]//div[@class="dx-checkbox-container"]`)).last();
     }
-
-//td[@aria-describedby="dx-col-445"]//div[contains(@class,dx-datagrid-checkbox-size)]
 }
